66494ef44373ceca400e0fdfc4717b4349f8e5c6
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2022-08-21 14:11:32 +0630

    init store front

---
+++ .storybook/main.js
@@ -0,0 +1,45 @@
+const path = require("path")
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
+  features: {
+    storyStoreV7: true,
+  },
+  framework: "@storybook/react",
+  core: {
+    builder: {
+      name: "webpack5",
+      options: {
+        lazyCompilation: true,
+        fsCache: true,
+      },
+    },
+  },
+  webpackFinal: (config) => {
+    config.resolve.plugins = config.resolve.plugins || []
+    config.resolve.plugins.push(
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      })
+    )
+
+    return config
+  },
+}
