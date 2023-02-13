f0ada4386f7626d49299b7d607b30b6cb2d5c073
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2023-02-13 09:54:38 +0630

    remove: hero and footer cta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "@modules/common/components/head"
 import FeaturedProducts from "@modules/home/components/featured-products"
-import Hero from "@modules/home/components/hero"
+// import Hero from "@modules/home/components/hero"
 import Layout from "@modules/layout/templates"
 import { ReactElement } from "react"
 import { NextPageWithLayout } from "types/global"
@@ -12,7 +12,7 @@ const Home: NextPageWithLayout = () => {
         title="Home"
         description="Shop all available models only at the ACME. Worldwide Shipping. Secure Payment."
       />
-      <Hero />
+      {/* <Hero /> */}
       <FeaturedProducts />
     </>
   )
