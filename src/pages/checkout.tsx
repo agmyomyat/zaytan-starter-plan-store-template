1adf7125055ba2bdf3dafbf9dc3eca867ee34609
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2023-02-14 11:22:26 +0630

    Add: Layout

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,13 +1,19 @@
 import CheckoutTemplate from "@modules/checkout/templates"
 import Head from "@modules/common/components/head"
+import Layout from "@modules/layout/templates"
+import { ReactElement } from "react"
+import { NextPageWithLayout } from "types/global"
 
-const Checkout = () => {
+const Checkout: NextPageWithLayout = () => {
   return (
     <>
       <Head title="Checkout" />
       <CheckoutTemplate />
     </>
   )
+}
+Checkout.getLayout = (page: ReactElement) => {
+  return <Layout>{page}</Layout>
 }
 
 export default Checkout
