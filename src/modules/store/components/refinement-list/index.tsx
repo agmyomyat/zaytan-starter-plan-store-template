52f1a6e52a03663b946fd8be33fe6c28ac3e182a
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2023-03-07 12:13:49 +0630

    change: collection hidden in small view port

--- src/modules/store/components/refinement-list/index.tsx
+++ src/modules/store/components/refinement-list/index.tsx
@@ -45,7 +45,7 @@ const RefinementList = ({
   }
 
   return (
-    <div>
+    <div className="small:block hidden">
       <div className="px-8 py-4  small:pr-0 small:pl-8 small:min-w-[250px]">
         <div className="flex gap-x-3 small:flex-col small:gap-y-3">
           <span className="text-base-semi">Collections</span>
