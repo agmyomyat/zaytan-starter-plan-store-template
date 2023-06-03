cc22bdb9814f5be7387dca45bc0ec12e6da1f760
urnotalone2 <54217097+agmyomyat@users.noreply.github.com>
2023-06-03 14:41:56 +0630

    chore:remove discount

--- src/modules/checkout/templates/checkout-summary/index.tsx
+++ src/modules/checkout/templates/checkout-summary/index.tsx
@@ -1,4 +1,4 @@
-import DiscountCode from "@modules/checkout/components/discount-code"
+// import DiscountCode from "@modules/checkout/components/discount-code"
 // import GiftCard from "@modules/checkout/components/gift-card"
 import PaymentButton from "@modules/checkout/components/payment-button"
 import CartTotals from "@modules/common/components/cart-totals"
@@ -17,9 +17,9 @@ const CheckoutSummary = () => {
         <CartTotals cart={cart} />
         <PaymentButton paymentSession={cart?.payment_session} />
       </div>
-      <div className="p-6 bg-white">
-        <DiscountCode cart={cart} />
-      </div>
+      {/* <div className="p-6 bg-white"> */}
+      {/* <DiscountCode cart={cart} /> */}
+      {/* </div> */}
       {/* <GiftCard cart={cart} /> */}
     </div>
   )
